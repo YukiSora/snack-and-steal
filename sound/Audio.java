9c5663036d896eed2d9b5651ab5310381dced4d4
yukisora <yukisora2013@gmail.com>
2016-05-31 17:57:53 +0800

    Audio

---
+++ sound/Audio.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Audio implements Runnable{
+    private Clip clip;
+    private Thread thread;
+
+    Audio(File file) throws IOException {
+        try {
+            clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(file));
+        } catch (LineUnavailableException | UnsupportedAudioFileException e) {
+            close();
+            throw new IOException("Unavailable audio");
+        }
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void play() {
+        run();
+    }
+
+    public void loop() {
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop() {
+        clip.stop();
+    }
+
+    public void close() {
+        try {
+            clip.close();
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void run() {
+        play();
+    }
+}
