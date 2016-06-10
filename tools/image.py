9bb7b0aa3ef2378c21d5720e768b664f630f1d7e
yukisora <yukisora2013@gmail.com>
2016-06-10 18:35:35 +0800

    tools

---
+++ tools/image.py
@@ -0,0 +1,30 @@
+from PIL import Image
+from PIL import ImageDraw
+
+image = Image.open('walkable.png')
+
+# poi = Image.open('code.jpg')
+# draw = ImageDraw.Draw(poi)
+
+# for h in range(0, image.size[1]):
+#     for w in range(0, image.size[0]):
+#         pixel = image.getpixel((w, h))
+#         if pixel == (255, 255, 255, 0):
+#             print(0, end = "")
+#         else:
+#             print(1, end="")
+#    print()
+
+# image test
+poi = Image.open('code.jpg')
+draw = ImageDraw.Draw(poi)
+
+for h in range(0, image.size[1]):
+    for w in range(0, image.size[0]):
+        pixel = image.getpixel((w, h))
+        if not pixel == (255, 255, 255, 0):
+            draw.point((w, h), fill=(0, 0, 0, 0))
+        else:
+            draw.point((w, h), fill=pixel)
+
+poi.save('code.jpg', 'jpeg');
