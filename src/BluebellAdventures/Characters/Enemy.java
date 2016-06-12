f7af3a70654c605f979c98e8ac4393a308c0b6f0
yukisora <yukisora2013@gmail.com>
2016-06-12 14:55:23 +0800

    enemy detection area

--- src/BluebellAdventures/Characters/Enemy.java
+++ src/BluebellAdventures/Characters/Enemy.java
@@ -2,6 +2,7 @@ package BluebellAdventures.Characters;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.geom.Arc2D;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -36,6 +37,20 @@ public class Enemy extends Sprite {
 		if (getVisible()) {
 			GameMap map = GameMap.getInstance();
 			g.drawImage(getImage(), map.getPosition().getX() + getPosition().getX(), map.getPosition().getY() + getPosition().getY(), null);
+			int dir = 0;
+			if (getDirection()[0] == 1) {
+				dir = 3;
+			}
+			if (getDirection()[0] == -1) {
+				dir = 1;
+			}
+			if (getDirection()[1] == 1) {
+				dir = 2;
+			}
+			if (getDirection()[1] == -1) {
+				dir = 0;
+			}
+			g.fill(new Arc2D.Double(map.getPosition().getX() + getPosition().getX(), map.getPosition().getY() + getPosition().getY(), 200, 200, 30 + 90 * dir, 120, Arc2D.PIE));
 		}
 	}
 
