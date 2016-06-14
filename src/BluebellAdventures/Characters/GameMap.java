d565a9be9dd5d693031749f00583f844f1ee0948
bubbleseal <bubbleseal@gmail.com>
2016-06-14 17:01:53 +0800

    add \r for windows

--- src/BluebellAdventures/Characters/GameMap.java
+++ src/BluebellAdventures/Characters/GameMap.java
@@ -67,7 +67,6 @@ public class GameMap extends Sprite {
 		try (FileInputStream in = new FileInputStream(filename)) {
 			int c;
 			while ((c = in.read()) != -1) {
-				if (c == '\n') {
 					i++;
 					j = 0;
 				}
