2324948d386a7cd2a904377b9cae778014d061df
Delacrix Morgan <Delacrix@Tecks-MacBook-Pro.local>
2016-06-26 00:29:43 +0800

    Enemy is doing cha cha

--- src/BluebellAdventures/Characters/Enemy.java
+++ src/BluebellAdventures/Characters/Enemy.java
@@ -63,9 +63,11 @@ public class Enemy extends Sprite {
             
             AffineTransform tx = new AffineTransform();
             tx.rotate(Math.PI/dir, getImage().getWidth()/2, getImage().getHeight()/2);
+            Graphics2D g2d = getImage().createGraphics();
+            g2d.drawImage(getImage(), tx, null);
 
-            AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
-            g.drawImage(op.filter(getImage(), null), map.getPosition().getX() + getPosition().getX(), map.getPosition().getY() + getPosition().getY(), null);
+            //AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+            //g.drawImage(op.filter(getImage(), null), map.getPosition().getX() + getPosition().getX(), map.getPosition().getY() + getPosition().getY(), null);
             
             g.drawImage(getImage(), map.getPosition().getX() + getPosition().getX(), map.getPosition().getY() + getPosition().getY(), null);
             //g.fill(new Arc2D.Double(map.getPosition().getX() + getPosition().getX(), map.getPosition().getY() + getPosition().getY(), 200, 200, 30 + 90 * dir, 120, Arc2D.PIE));
