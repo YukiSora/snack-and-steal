5c46ff034551e892dfc6b63d95c3e1c7fe82ed1e
yukisora <yukisora2013@gmail.com>
2016-06-25 17:53:46 +0800

    create menu scene

--- src/BluebellAdventures/Main.java
+++ src/BluebellAdventures/Main.java
@@ -10,6 +10,7 @@ import BluebellAdventures.Actions.ChangeScene;
 import BluebellAdventures.Actions.SelectCharacter;
 import BluebellAdventures.Actions.Quit;
 import BluebellAdventures.CreateScenes.CreateLoadingScene;
+import BluebellAdventures.CreateScenes.CreateMenuScene;
 
 import Megumin.Actions.Action;
 import Megumin.Actions.MouseCrash;
@@ -65,7 +66,7 @@ public class Main {
         interact.addEvent(KeyEvent.VK_ESCAPE, Interact.ON_KEY_CLICK, system, new Quit());
 
         //create scene
-        Scene menu = createMenuScene();
+        Scene menu = CreateMenuScene.createMenuScene();
         Scene characterSelection = createCharacterSelectionScene();
 
         //menu action
@@ -87,33 +88,6 @@ public class Main {
         } catch (InterruptedException e) {
             System.out.println(e);
         }
-    }
-
-    public static Scene createMenuScene() throws IOException {
-        //init sprite
-        Sprite singlePlayer = new Sprite("resource/image/tag_single.png", new Point(200, 100));
-        Sprite multiPlayer = new Sprite("resource/image/tag_multi.png", new Point(200, 250));
-        Sprite setting = new Sprite("resource/image/tag_setting.png", new Point(200, 400));
-        Sprite exit = new Sprite("resource/image/tag_quit.png", new Point(200, 550));
-        Sprite background = new Sprite("resource/image/menu_bg.jpeg");
-        singlePlayer.setName("single player");
-        exit.setName("exit");
-
-        //init layer
-        Layer tabLayer = new Layer();
-        tabLayer.addSprite(singlePlayer);
-        tabLayer.addSprite(multiPlayer);
-        tabLayer.addSprite(setting);
-        tabLayer.addSprite(exit);
-        Layer mapLayer = new Layer();
-        mapLayer.addSprite(background);
-
-        //init scene
-        Scene menu = new Scene();
-        menu.addLayer(tabLayer);
-        menu.addLayer(mapLayer, 0);
-
-        return menu;
     }
 
     public static Scene createCharacterSelectionScene() throws IOException {
