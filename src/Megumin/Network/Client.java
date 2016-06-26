fafb733718740ce417df1f502f84d10a864e8164
yukisora <yukisora2013@gmail.com>
2016-06-26 15:12:07 +0800

    network

---
+++ src/Megumin/Network/Client.java
@@ -0,0 +1,94 @@
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class Client <T> {
+    private int port;
+    private String ip;
+
+    Client(String ip, int port) throws InterruptedException, UnknownHostException, IOException {
+        this.ip = ip;
+        this.port = port;
+
+        Socket client = new Socket(ip, port);
+        Thread clientThread = new Thread(new OutputThread <T>(new ObjectOutputStream(client.getOutputStream()), T.class));
+        clientThread.start();
+        clientThread.join();
+        Thread serverThread = new Thread(new InputThread <T>(new ObjectInputStream(client.getInputStream())));
+        serverThread.start();
+        serverThread.join();
+    }
+
+    public static void main(String[] args) {
+        try {
+            Client <String> client = new Client<>("localhost", 2333);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        } catch (UnknownHostException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private class InputThread <T> implements Runnable {
+        private ObjectInputStream in;
+
+        InputThread(ObjectInputStream in) {
+            this.in = in;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println((T)in.readObject());
+                }
+            } catch (ClassNotFoundException e) {
+                System.out.println(e);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    private class OutputThread <T> implements Runnable {
+        private ObjectOutputStream out;
+        private Class cls;
+
+        OutputThread(ObjectOutputStream out, Class cls) {
+            this.out = out;
+            this.cls = cls;
+        }
+
+        @Override
+        public void run() {
+            Scanner input = new Scanner(System.in);
+
+            try {
+                while (true) {
+                    String s = input.nextLine();
+                    Constructor constructor = cls.getDeclaredConstructor(String.class);
+                    constructor.setAccessible(true);
+
+                    Object obj = (T) constructor.newInstance(s);
+                    T t = (T) obj;
+
+                    out.writeObject(t);
+                    out.flush();
+                }
+            } catch (IOException | InstantiationException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
---
+++ src/Megumin/Network/Server.java
@@ -0,0 +1,109 @@
+import java.net.Socket;
+import java.net.ServerSocket;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import java.util.Iterator;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Server implements Runnable {
+    private int port;
+    private CopyOnWriteArrayList<Client> clients;
+    private ExecutorService pool;
+
+    Server(int port) {
+        this.port = port;
+        clients = new CopyOnWriteArrayList<>();
+        pool = Executors.newFixedThreadPool(50);
+        new Thread(this).start();
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(2333);
+    }
+
+    @Override
+    public void run() {
+        try (ServerSocket server = new ServerSocket(port)) {
+            while (true) {
+                try {
+                    Client client = new Client(server.accept());
+                    clients.add(client);
+                    Callable<Void> task = new ClientThread(client);
+                    pool.submit(task);
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private class Client {
+        private Socket client;
+        private ObjectInputStream in;
+        private ObjectOutputStream out;
+
+        Client(Socket client) throws IOException {
+            this.client = client;
+            in = new ObjectInputStream(client.getInputStream());
+            out = new ObjectOutputStream(client.getOutputStream());
+        }
+
+        public Socket getSocket() {
+            return client;
+        }
+
+        public ObjectInputStream getIn() {
+            return in;
+        }
+
+        public ObjectOutputStream getOut() {
+            return out;
+        }
+    }
+
+    private class ClientThread implements Callable<Void> {
+        private Client client;
+
+        ClientThread(Client client) {
+            this.client = client;
+        }
+
+        @Override
+        public Void call() {
+            try {
+                while (true) {
+                    T data = (T)client.getIn().readObject();
+                    System.out.println(data);
+                    Iterator it = clients.iterator();
+                    while (it.hasNext()) {
+                        Client client = (Client)it.next();
+                        if (!client.getSocket().equals(this.client.getSocket())) {
+                            client.getOut().writeObject(data);
+                        }
+                    }
+                }
+            } catch (ClassNotFoundException e) {
+                System.out.println(e);
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+                clients.remove(this);
+                try {
+                    client.getSocket().close();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+
+            return null;
+        }
+    }
+}
