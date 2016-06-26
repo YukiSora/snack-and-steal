c304780c29ebe20be272fafc450bd075f9a89f2a
bubbleseal <bubbleseal@gmail.com>
2016-06-27 02:18:29 +0800

    Updated fridge lock

--- src/BluebellAdventures/Actions/CrashFridge.java
+++ src/BluebellAdventures/Actions/CrashFridge.java
@@ -15,15 +15,25 @@ import Megumin.Point;
 public class CrashFridge extends Effect {
     @Override
     public void update(Sprite sprite) {
-        AudioEngine.getInstance().play("fridge");
-        try {
-            Sprite fridge = Director.getInstance().getScene().getSpriteByName("fridge");
-            fridge.setImage("resource/image/fridge_open.png");
-            fridge.setPosition(new Point(3387, 2420));
+        //Unlock fridge if player has key
+        if (((MovableObject) this.getSprite()).getLock() == true) {
+            Character player = ((Character) Director.getInstance().getScene().getSpriteByName("player"));
+            int key = player.getKey();
+            if (key != 0) {
+                player.setKey(key - 1);
+                ((MovableObject) this.getSprite()).setLock(false);
+            }
         }
-        catch(IOException e) {
+        //Open fridge if unlocked
+        try {
+            if (((MovableObject) this.getSprite()).getLock() == false) {
+                Sprite fridge = Director.getInstance().getScene().getSpriteByName("fridge");
+                AudioEngine.getInstance().play("fridge");
+                fridge.setImage("resource/image/fridge_open.png");
+                fridge.setPosition(new Point(3387, 2420));
+            }
+        } catch (IOException e) {
             System.exit(1);
         }
-        ((MovableObject)this.getSprite()).setLock(false);
     }
 }
