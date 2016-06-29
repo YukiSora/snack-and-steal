4ccefd22885cc04cd93162729ed2f3078cbbbb0f
yukisora <yukisora2013@gmail.com>
2016-06-29 18:09:43 +0800

    update driver

--- src/BluebellAdventures/CharacterDriver.java
+++ src/BluebellAdventures/CharacterDriver.java
@@ -16,28 +16,22 @@ public class CharacterDriver {
             System.out.println(snack.getScore());
 
             Enemy enemy = new Enemy(filename).setAttack(10)
-                                                .setDetectionRange(120)
                                                 .setSpeed(100);
             System.out.println(enemy.getAttack());
-            System.out.println(enemy.getDetectionRange());
             System.out.println(enemy.getSpeed());
 
             Character character = new Character(filename).setHp(100)
                                                             .setMp(100)
                                                             .setChargeBar(10)
-                                                            .setLives(3)
                                                             .setSpeed(100)
                                                             .setUnlockSpeed(100)
-                                                            .setDetectionRange(100)
                                                             .setAttackScore(0)
                                                             .setSnackScore(0);
             System.out.println(character.getHp());
             System.out.println(character.getMp());
             System.out.println(character.getChargeBar());
-            System.out.println(character.getLives());
             System.out.println(character.getSpeed());
             System.out.println(character.getUnlockSpeed());
-            System.out.println(character.getDetectionRange());
             System.out.println(character.getAttackScore());
             System.out.println(character.getSnackScore());
         } catch (IOException e) {
